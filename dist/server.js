55487414a6dfb7ba841d1c5f67b69a3319992f37
Daniel Molnar <molnard.work@gmail.com>
2023-02-28 14:56:17 +0100

    deep dive into client operations

--- dist/server.js
+++ dist/server.js
@@ -13,11 +13,88 @@ const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        /*const x = await prisma.testUser.create({
-            data: {
-                name: 'Kyle'
+        const userRelations = yield prisma.testUser.findFirst({
+            where: {
+                writtenPosts: {
+                    every: {
+                        createdAt: new Date(),
+                        title: "Test"
+                    }
+                }
             }
-        })*/
+        });
+        const user = yield prisma.testUser.findMany({
+            where: {
+                AND: [
+                    {
+                        age: {
+                            lt: 10
+                        }
+                    },
+                    {
+                        email: {
+                            contains: "@prisma.com",
+                            endsWith: ".com"
+                        }
+                    }
+                ],
+                OR: [
+                    {
+                        age: {
+                            lt: 10
+                        }
+                    },
+                    {
+                        email: {
+                            contains: "@prisma.com",
+                            endsWith: ".com"
+                        }
+                    }
+                ],
+                NOT: [
+                    {
+                        age: {
+                            lt: 10
+                        }
+                    },
+                    {
+                        email: {
+                            contains: "@prisma.com",
+                            endsWith: ".com"
+                        }
+                    }
+                ],
+                name: {
+                    not: "Adam",
+                    in: ["Alex", "Sally"],
+                    notIn: ["Bela", "Andras"],
+                    equals: "Sally",
+                    endsWith: "X"
+                },
+                age: {
+                    lt: 10
+                },
+                email: {
+                    contains: "@prisma.com",
+                    endsWith: ".com"
+                }
+            },
+            orderBy: {
+                age: "asc"
+            },
+            skip: 1,
+            take: 2,
+            distinct: ["name"] // if there are more users with same name I get the one
+        });
+        const userCombination = yield prisma.testUser.findUnique({
+            where: {
+                age_name: {
+                    name: "test@prisma.com",
+                    age: 12
+                }
+            }
+        });
+        console.log(user);
     });
 }
 main();
--- server.ts
+++ server.ts
@@ -2,11 +2,89 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    /*const x = await prisma.testUser.create({
-        data: {
-            name: 'Kyle'
+    const userRelations = await prisma.testUser.findFirst({
+        where: {
+            writtenPosts: {
+                every: {
+                    createdAt: new Date()
+                }
+            }
         }
-    })*/
+    })
+
+    const user = await prisma.testUser.findMany({
+        where: {
+            AND: [
+                {
+                    age: {
+                        lt: 10
+                    }
+                },
+                {
+                    email: {
+                        contains: "@prisma.com",
+                        endsWith: ".com"
+                    }
+                }
+            ],
+            OR: [
+                {
+                    age: {
+                        lt: 10
+                    }
+                },
+                {
+                    email: {
+                        contains: "@prisma.com",
+                        endsWith: ".com"
+                    }
+                }
+            ],
+            NOT: [
+                {
+                    age: {
+                        lt: 10
+                    }
+                },
+                {
+                    email: {
+                        contains: "@prisma.com",
+                        endsWith: ".com"
+                    }
+                }
+            ],
+            name: {
+                not: "Adam",
+                in: ["Alex", "Sally"],
+                notIn: ["Bela", "Andras"],
+                equals: "Sally",
+                endsWith: "X"
+            },
+            age: {
+                lt: 10
+            },
+            email: {
+                contains: "@prisma.com",
+                endsWith: ".com"
+            }
+        },
+        orderBy: {
+            age: "asc"
+        },
+        skip: 1, // skip the first one
+        take: 2, // pagination (how many result I want)
+        distinct: ["name"] // if there are more users with same name I get the one
+    })
+    const userCombination = await prisma.testUser.findUnique({
+        where: {
+            age_name: {
+                name: "test@prisma.com",
+                age: 12
+            }
+        }
+    })
+
+    console.log(user);
 }
 
 main();
